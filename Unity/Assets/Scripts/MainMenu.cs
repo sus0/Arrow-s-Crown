ff0e6a0765cd1749c651ffe4c09196b77f2082c5
sus0 <susie_su94@hotmail.com>
2015-02-17 21:49:31 -0500

    Change the structure to make it custom (i.e. apply to multiple texts).

--- Unity/Assets/Scripts/MainMenu.cs
+++ Unity/Assets/Scripts/MainMenu.cs
@@ -4,10 +4,6 @@ using UnityEngine.UI;
 
 [ExecuteInEditMode]
 public class MainMenu : MonoBehaviour {
-	public Text text;
-	void OnStart(){
-
-	}
 	public void StartButtonOnClick(){
 		Application.LoadLevel("MainScene");
 	}
@@ -18,11 +14,17 @@ public class MainMenu : MonoBehaviour {
 	public void BackButtonOnClick(){
 		Application.LoadLevel("MainMenu");
 	}
+	public void ExitButtonOnClick(){
+		Application.Quit();
+	}
 
 	public void TextColorChangeToWhite() {
-		text.color = Color.white;
+		GetComponent<Text>().color = Color.white;
 	}
 	public void TextColorChangeToRed() {
-		text.color = new Color32(129, 9, 9, 255);
+		GetComponent<Text>().color = new Color32(129, 9, 9, 255);
+	}
+	public void TextColorChangeToYellow() {
+		GetComponent<Text>().color = new Color32(247, 213, 19, 255);
 	}
 }
